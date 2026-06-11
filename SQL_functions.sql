ec55a010dc5a8ecfb5f248a0e2b9e4f5664a9951
Stephen-A-07 <sa0388701@gmail.com>
2026-06-11 13:54:27 +0530

    Add SQL scripts for numeric and date-time functions with examples

--- SQL_functions.sql
+++ SQL_functions.sql
@@ -51,10 +51,79 @@ FROM customers;
 SELECT SUBSTRING(firstname,2,LENGTH(firstname))
 FROM customers;
 
+------------------------------------------------------------
+--NUMERIC FUNCTIONS
+
+--ROUND
+
+SELECT ROUND(3.521);
+
+--ABS
+SELECT ABS(-34);
+
+--------------------------------------------------------
+
+--DATE-TIME FUNCTIONS
+
+--NOW
+
+SELECT orderid,creationtime,NOW() Today FROM sales.orders;
+
+--PART EXRACTION
+
+--day,date,year functions
+
+SELECT 
+	EXTRACT(DAY FROM creationtime) cday,
+	EXTRACT(MONTH FROM creationtime) cmonth,
+	EXTRACT(YEAR FROM creationtime) cyear
+FROM orders;
+
+--to get the day names
+SELECT TO_CHAR(creationtime,'day') FROM orders;
+SELECT TO_CHAR(creationtime,'month') FROM orders;
+
+--truncat
+/*everything after month will be reseted*/
+SELECT DATE_TRUNC('month', creationtime)
+FROM orders;
+
+/*return number of ordes for each mont*/
+
+SELECT
+	DATE_TRUNC('month',creationtime),count(*)
+FROM orders GROUP BY DATE_TRUNC('month',creationtime);
+
+/*select all the orders place dduring the month of feb */
+
+SELECT * 
+	FROM orders 	
+WHERE EXTRACT(month FROM creationtime)::int = 2;
+
+--FORMATING AND CASTING
+--changing the format
+
+SELECT TO_CHAR(creationtime, 'DD Day Mon "Q1" YYYY HH:mm:ss:AM') FROM orders;
+
+--cast or ::
+
+SELECT CAST('123' AS INT);
+SELECT '123' :: INT;
 
 
 
 
-				  
+
+
+
+
+
+
+
+
+
+
+
+
 
 
