4b7f57f8aac0f13b304a86a148a8fa7a2650312d
Stephen-A-07 <sa0388701@gmail.com>
2026-06-16 14:30:34 +0530

    Add SQL window functions for advanced analytics and aggregation

---
+++ SQL_WIndow functions.sql
@@ -0,0 +1,112 @@
+--WINDOW FUNCTIONS
+
+SET search_path TO 'sales';
+
+
+/* limits of group by */
+-- SELECT 
+-- 	orderid,
+-- 	orderdate,
+-- 	productid,
+-- 	SUM(sales) sum_sales 
+-- FROM orders GROUP BY productid;
+
+
+SELECT 
+	orderid,
+	orderdate,
+	productid,
+	SUM(sales) OVER(PARTITION BY productid) sum_sales 
+FROM orders;
+
+--SYNTAX
+
+-- window fuction() over-- partition by,order by,frame clause
+
+SELECT 
+	orderid,
+	orderdate,
+	productid,
+	SUM(sales) OVER() sum_sales, 
+	SUM(sales) OVER(PARTITION BY productid) salesbyproduct,
+	SUM(sales) OVER(PARTITION BY productid,orderstatus) salesbyproductstatus
+FROM orders;
+
+--with rank oredr by is must 
+SELECT 
+	orderid,
+	orderdate,
+	sales,
+	RANK() OVER(ORDER BY sales DESC) Ranksales 
+FROM orders;
+
+
+--partition is used to divide a cols into different windows 
+--frame is use to get rows inside the windows
+--used only with order by
+
+
+-- here first the partition is done it is divided into 2 
+-- delivered and shipped and we order it by date and then sum the values
+-- based on the condition of the fram clause of one window at a time
+SELECT 
+	orderid,
+	orderdate,
+	sales,
+	orderstatus,
+	SUM(sales) OVER(PARTITION BY orderstatus ORDER BY orderdate ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING) Ranksales 
+FROM orders;
+
+-- order by always uses a fram default frame 
+-- ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+
+
+-- RULES for window function
+-- 1)can be used only in select and order by we cannot use filter in window function like where and group by 
+-- 2)we cannot have nested wf like one wf inside another
+-- 	SUM(SUM(sales) OVER(PARTITION BY orderstatus)) OVER(PARTITION BY orderstatus)
+
+-- 3)the window f will be executed after the where clause
+-- 4)we can use the wf together with group by in the same query only if the same columns are used
+
+-------------------------------------------------------------------------------------
+
+--window aggregate functions
+--count() any data type and the rest integer only
+
+--COUNT()
+--count numbers only non null values
+
+--count(1) ==  count(*) same values and same performens
+
+SELECT 
+	orderid,
+	orderdate,
+	customerid,
+	COUNT(*) OVER(PARTITION BY customerid) orderbycustomers
+FROM orders;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
